1a2b504c7b9c198063aad8f81a366906b29bf112
Mohamed Youssouf <mohamedyoussoufali@gmail.com>
2022-11-02 10:16:13 +0300

    Complete the header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        xl: '1156px',
+      },
+    },
+    fontSize: {
+      base: '16px',
+      '4xl': '90.44px',
+    },
+    extend: {
+      colors: {
+        bg: '#CCCCDB',
+        primary: '#2A2645'
+      }
+    },
   },
   plugins: [],
 }
